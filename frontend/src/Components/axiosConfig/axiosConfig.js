e6d43e40452111ad467368d8b7d66ea1918dd17d
ashma <ashmaanirudhan@gmail.com>
2025-05-02 01:20:13 +0530

    axios timeout update

--- frontend/src/Components/axiosConfig/axiosConfig.js
+++ frontend/src/Components/axiosConfig/axiosConfig.js
@@ -3,7 +3,8 @@ const token = localStorage.getItem('@token');
 const axiosInstance = axios.create({
     baseURL: 'https://freelance-platform-finalproject.onrender.com/api',
  //baseURL: 'http://localhost:5000/api',
-    timeout: 5000,
+ timeout: 10000, 
+
         headers: {
            'Authorization': token ? `Bearer ${token}` : '',
             'Accept': 'application/json'
