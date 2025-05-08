dbd1f794da1a795278ead493af6bf610d20deb51
ashma <ashmaanirudhan@gmail.com>
2025-05-09 02:05:03 +0530

    changes in api

--- frontend/src/apiUtils/userApi.js
+++ frontend/src/apiUtils/userApi.js
@@ -2,7 +2,7 @@ import axiosInstance from "../Components/axiosConfig/axiosConfig";
  export const userLogin = async (payload) =>{
     const {email,password} = payload;
     if(email && password){
-        const result = await axiosInstance.post('auth/login',payload);
+        const result = await axiosInstance.post('https://freelance-platform-finalproject.vercel.app/auth/login',payload);
      
         return {
             success :true,
